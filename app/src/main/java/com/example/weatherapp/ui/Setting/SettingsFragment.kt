ca8befa066bd60a3b782c00d53d5cc928e08ebc9
romina <saljooghian.r@ggmail.com>
2021-01-20 17:01:32 +0330

    project

---
+++ app/src/main/java/com/example/weatherapp/ui/Setting/SettingsFragment.kt
@@ -0,0 +1,19 @@
+package com.example.weatherapp.ui.Setting
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.PreferenceFragmentCompat
+import com.example.weatherapp.R
+
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        (activity as? AppCompatActivity)?.supportActionBar?.title = "Settings"
+        (activity as? AppCompatActivity)?.supportActionBar?.subtitle = null
+    }
+}
