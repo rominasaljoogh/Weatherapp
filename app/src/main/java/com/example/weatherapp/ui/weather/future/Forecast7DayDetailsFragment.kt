93ec091f3da61ca86aca81adb79128352aa43691
romina <saljooghian.r@ggmail.com>
2021-01-30 17:26:27 +0330

    project

--- app/src/main/java/com/example/weatherapp/ui/weather/future/Forecast7DayDetailsFragment.kt
+++ app/src/main/java/com/example/weatherapp/ui/weather/future/Forecast7DayDetailsFragment.kt
@@ -10,6 +10,8 @@ import androidx.navigation.Navigation
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.weatherapp.R
+import com.example.weatherapp.data.provider.UnitProviderImpl
+import com.example.weatherapp.data.provider.UnitSystem
 import kotlinx.android.synthetic.main.fragment_current_weather.*
 import kotlinx.android.synthetic.main.fragment_forecast7_day_details.*
 import kotlinx.android.synthetic.main.fragment_forecast7_day_details.imageView_condition_icon
@@ -24,6 +26,8 @@ class Forecast7DayDetailsFragment : Fragment() {
 
     private lateinit var  navController : NavController
 
+    var unit : Boolean? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -35,17 +39,40 @@ class Forecast7DayDetailsFragment : Fragment() {
 
         navController = Navigation.findNavController(view)
 
-        textView_temperature.text = args.Daily.temp.day.toString()
-        textView_min_max_temperature.text = "Max: ${args.Daily.temp.max} Min: ${args.Daily.temp.min}"
+        val unitProvider = UnitProviderImpl(requireContext()).getUnitSystem()
+        if(unitProvider == UnitSystem.METRIC) unit=true
+
+
+        updateui()
+
+    }
+
+    private fun updateui(){
+
+        updateTemperatures()
         textView_condition.text = args.Daily.weather[0].description
-        textView_wind.text = "Wind: ${args.Daily.windSpeed} "
+        updateWind()
         textView_precipitation.text = "Precipitation: ${args.Daily.rain} "
         textView_pressure.text = "Pressure: ${args.Daily.pressure}"
         textView_uv.text = "UV: ${args.Daily.uvi}"
         Glide.with(this@Forecast7DayDetailsFragment)
             .load("http://openweathermap.org/img/wn/${args.Daily.weather[0].icon}@2x.png")
             .into(imageView_condition_icon)
+    }
 
+    private fun chooseUnit(metric: String, imperial: String): String {
+        return if (unit == true) metric else imperial
+    }
+
+    private fun updateTemperatures() {
+        val unitAbbreviation = chooseUnit("°C", "°F")
+        textView_temperature.text = "${args.Daily.temp.day} $unitAbbreviation"
+        textView_min_max_temperature.text = "Max: ${args.Daily.temp.max} $unitAbbreviation Min: ${args.Daily.temp.min} $unitAbbreviation"
+    }
+
+    private fun updateWind() {
+        val unitAbbreviation = chooseUnit("m/s", "mph")
+        textView_wind.text = "Wind: ${args.Daily.windSpeed} $unitAbbreviation"
     }
 
 }
